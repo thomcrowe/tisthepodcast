966deadb85a328587f656c8b9a3baa5f163571a5
Ubaid Badar <m.ubaidbadar@gmail.com>
2023-01-05 21:32:43 +0500

    resolved-production-error

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,7 +2,7 @@ import { Container } from '@/components/Container'
 import Image from 'next/image'
 
 const LK = ({ className = '', username = '', brand = '' }) => (
-    <a className={`btn-icon -my-[0.5em] ${className}`} href={`https://${brand}.com/${username}`} target="_blank"><i className={`fa-brands fa-${brand}`} /></a>
+    <a className={`btn-icon -my-[0.5em] ${className}`} rel="noreferrer" href={`https://${brand}.com/${username}`} target="_blank"><i className={`fa-brands fa-${brand}`} /></a>
 )
 
 const Blog = ({ title = '', url = '', children }) => (
@@ -15,27 +15,27 @@ const Blog = ({ title = '', url = '', children }) => (
     </div>
 )
 
-export default () => (
+const About = () => (
     <>
         <div className="pt-16 pb-12 sm:pb-4 lg:pt-12 flex-1">
             <Container>
                 <div className='grid'>
                     <div>
                         <h1 className="text-2xl uppercase mb-6 font-bold leading-7 text-slate-900">About Tis Project</h1>
-                        <p>Welcome to Tis the Podcast, the podcast that's determined to keep the spirit of Christmas alive 365 days a year! Join us and our community as we talk all things Christmas! So what can you expect from this group of misfit toys? Every week we're going to look at a different Christmas movie, special, or TV episode. As we talk, we'll share our reviews, insights, and witty banter. As our reviews grow, you can find the canonical ranking of Christmas movies here on our site. Really, what more could you want out of a Christmas podcast?</p>
+                        <p>Welcome to Tis the Podcast, the podcast that&apos;s determined to keep the spirit of Christmas alive 365 days a year! Join us and our community as we talk all things Christmas! So what can you expect from this group of misfit toys? Every week we&aposre going to look at a different Christmas movie, special, or TV episode. As we talk, we&apos;ll share our reviews, insights, and witty banter. As our reviews grow, you can find the canonical ranking of Christmas movies here on our site. Really, what more could you want out of a Christmas podcast?</p>
                     </div>
                     <h1 className="text-2xl mt-8 uppercase font-bold leading-7 text-slate-900">ABOUT THE ELVES</h1>
-                    <Blog title="ANTHONY CARUSO aka Santa's Head Elf"
+                    <Blog title="ANTHONY CARUSO aka Santa&apos;s Head Elf"
                         url='oLQz8thDQY?ixlib=js-3.7.0&s=6ff8176dfd3a551d3b0a8b98129ef4db'
                     >
-                        Anthony inherited his love (some might say obsession) for Christmas from his parents, specifically his mother. He firmly believes that Christmas is a feeling that should be celebrated year round, not just once a year. (Though he will admit the month of December itself is the best month of the year, and September-December is "The Most Wonderful Time of the Year".) Besides the holidays, Anthony is: an aspiring author; a proud geek; an animal lover; and a progressive, feminist, political nerd. In addition, he watches way too many movies and television shows, and is adept at dropping quotes from all of them into everyday conversation. He's also lucky enough to have three places on Earth he calls home - New York (USA), Newfoundland (Canada), and Newcastle (UK) - and he loves his wife and two cats more than anything else, including Christmas itself.
+                        Anthony inherited his love (some might say obsession) for Christmas from his parents, specifically his mother. He firmly believes that Christmas is a feeling that should be celebrated year round, not just once a year. (Though he will admit the month of December itself is the best month of the year, and September-December is &quot;The Most Wonderful Time of the Year&quot;.) Besides the holidays, Anthony is: an aspiring author; a proud geek; an animal lover; and a progressive, feminist, political nerd. In addition, he watches way too many movies and television shows, and is adept at dropping quotes from all of them into everyday conversation. He&apos;s also lucky enough to have three places on Earth he calls home - New York (USA), Newfoundland (Canada), and Newcastle (UK) - and he loves his wife and two cats more than anything else, including Christmas itself.
                         <LK brand='twitter' username='TheSandersonBro' className='text-[#1d9bf0]' />
                         <LK brand='instagram' className='instagram-icon -ml-[0.5em]' username='anthonyc929' />
                     </Blog>
                     <Blog title='Julia Colburn aka The Holiday Armadillo'
                         url='EPNB1MlDol?ixlib=js-3.7.0&s=14e31d4a100d2163619a58df2876c422'
                     >
-                        Julia is a people-loving Pollyanna with an affinity for useless knowledge and pop-culture trivia. She can often speak in Movie/TV Quotes and Gifs. With her amazing husband's support, she has made it her goal to raise their 4 kids to not be cotton-headed ninny muggins'.
+                        Julia is a people-loving Pollyanna with an affinity for useless knowledge and pop-culture trivia. She can often speak in Movie/TV Quotes and Gifs. With her amazing husband&apos;s support, she has made it her goal to raise their 4 kids to not be cotton-headed ninny muggins.
                         <LK brand='instagram' className='instagram-icon' username='hartnhevn' />
                     </Blog>
                     <Blog
@@ -51,3 +51,5 @@ export default () => (
         </div>
     </>
 )
+
+export default About;
