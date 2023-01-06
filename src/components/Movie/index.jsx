0a8096a8ed6326ac563e02222b860d2bde06861d
Ubaid Badar <m.ubaidbadar@gmail.com>
2023-01-06 13:23:39 +0500

    improved-codd

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -2,16 +2,12 @@ import Image from 'next/image';
 import { Container } from '../Container';
 import styles from './styles.module.scss';
 
-const Movie = ({ title = '', rank = '', src = '' }) => (
-    <div className={styles.root}>
-        <Container>
-            <div className='flex py-2 items-center font-bold gap-4 text-lg'>
-                <Image width={70} height={150} alt={title} src={`https://cdn.raster.app/tis-the-podcast/the-list/${src}`} />
-                <h2>{title}</h2>
-                <p className='ml-auto'>{rank}</p>
-            </div>
-        </Container>
-    </div>
+const Movie = ({ title = '', rank = '', src = '', index }) => (
+    <Container className={styles.root}>
+        <div className={styles.main} title={`${index}. ${title}`} rank={rank}>
+            <Image width={70} height={150} alt={title} src={`https://cdn.raster.app/tis-the-podcast/the-list/${src}`} />
+        </div>
+    </Container>
 )
 
 export default Movie;
