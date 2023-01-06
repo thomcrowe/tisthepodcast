5b4bc33e5244cf9cca3f7738d3b5a3527a7186f9
Thom Crowe <thom@thomcrowe.com>
2023-01-05 23:12:24 -0600

    expanding thelist and adding a favicon

--- src/pages/list.jsx
+++ src/pages/list.jsx
@@ -20,6 +20,15 @@ const List = () => (
             <Movie title='Scrooged' rank='8.967' src='lSxJjpihOS?ixlib=js-3.7.0&s=bf5757765028558b1ef09d454d34378a' />
             <Movie title='Shazam!' rank='8.64' src='kARMhrGOVz?ixlib=js-3.7.0&s=2af294e2cec8278fa2254e4b821251c1' />
             <Movie title='Home Alone' rank='8.50' src='U4nSp7AHh3?ixlib=js-3.7.0&s=e55e6210254e9886be3b68a3e46c89a1' />
+            <Movie title='Anna and the Apocalypse' rank='8.4267' src='L35gyYWu72?ixlib=js-3.7.0&s=c8fc2be38f1bab1b1bc4c88823d19f57' />
+            <Movie title='Joyeux Noel' rank='8.33' src='OrtofgS8Sy?ixlib=js-3.7.0&s=8fc8903cea530f28543bab7d52fc3a7a' />
+            <Movie title='The Santa Secret' rank='8.33' src='e58zgVohTk?ixlib=js-3.7.0&s=584e4d07a3e789f09f232b02ec44ba08' />
+            <Movie title='Noelle' rank='8.28' src='7x0TvtYdyj?ixlib=js-3.7.0&s=7e93bad1ee2df62c35a27ee00bf49157' />
+            <Movie title='Love Acutally' rank='8.25' src='AkjV6WM3mY?ixlib=js-3.7.0&s=79f3ceaea58301161d9a2c6af4fde813' />
+            <Movie title='Santa Claus: The Movie' rank='8.25' src='ndnOeXrfZb?ixlib=js-3.7.0&s=066a6036fc111671246795d57e32af99' />
+            <Movie title='Almost Christmas' rank='8.25' src='mt7C8yvBAO?ixlib=js-3.7.0&s=f2b8d661aa9dd528bb8e8c76f01b0d9b' />
+            <Movie title='One Royal Holiday' rank='8.25' src='zmmpJYsZ0q?ixlib=js-3.7.0&s=765e6ad9be3e8440a757eb564cf3c706' />
+
         </div>
     </>
 )
