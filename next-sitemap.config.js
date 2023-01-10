61832baf4d576a477b357e9b66230689219a9997
Thom Crowe <thom@thomcrowe.com>
2023-01-10 16:30:53 -0600

    addinga  sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+const config = {
+    siteUrl: 'https://tisthepodcast.com',
+    generateRobotsTxt: true, // (Optional parameter for creating robots.txt file)
+    // Other available options..
+  }
+ 
+module.exports = config;
