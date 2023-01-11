fef01ed0492829515852338c708bc651c5acc5b0
Thom Crowe <thom@thomcrowe.com>
2023-01-10 22:17:08 -0600

    fixing typo on about

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -11,7 +11,6 @@ export default function App({ Component, pageProps }) {
     <AudioProvider>
       <Layout>
         <Component {...pageProps} />
-        <Component {...pageProps} />
       <Analytics />
       </Layout>
     </AudioProvider>
